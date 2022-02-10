7fcfac71bc83cf166015f26ca4e0b073b1088530
Amvilina <diatimp@gmail.com>
2022-02-10 19:08:49 +0300

    add double, ivec2, dvec2 random generator

--- SDLtest/Game/Random.hpp
+++ SDLtest/Game/Random.hpp
@@ -1,11 +1,18 @@
 #pragma once
 #include <random>
+#include "iVec2.hpp"
+#include "dVec2.hpp"
 
 class Random{
 public:
     Random();
-    //double GetDouble(double min, double max);
+    double GetDouble(double min, double max);
+    dVec2 GetDoubleVec2(double min1, double max1, double min2, double max2);
+    dVec2 GetDoubleVec2(double min, double max);
+    
     int GetInt(int min, int max);
+    iVec2 GetIntVec2(int min1, int max1, int min2, int max2);
+    iVec2 GetIntVec2(int min, int max);
 private:
     std::mt19937 mt;
 };
